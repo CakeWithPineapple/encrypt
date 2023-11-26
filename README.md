9b750613ec452b7d0dfe67b189fb57d82f20a8ad
CakeWithPineapple <cwp121991@gmail.com>
2023-11-26 20:37:52 +0000

    v2.0.0

--- README.md
+++ README.md
@@ -10,21 +10,18 @@ npm install hikelibs-encrypt
 
 ## Usage
 ```typescript
-import v1, { v1Interface } from "hikelibs-encrypt";
-
-const keyAES = Buffer.from(v1.encryptXOR('randomKey', 42), 'utf-8');
-const keyXOR = 42;
+import { v2 } from "hikelibs-encrypt";
 
 const textToEncrypt = 'Hello, World!';
 
-const encryptedAES = encryptionModule.encryptAES(textToEncrypt, keyAES);
-const decryptedAES = encryptionModule.encryptAES(encryptedAES, keyAES);
+const encryptedAES = v2('encrypt', 'aes', { string: textToEncrypt, key: keyAES });
+const decryptedAES = v2('decrypt', 'aes', { string: encryptedAES, key: keyAES });
 
-const encryptedBase64 = encryptionModule.encryptBase64(textToEncrypt);
-const decryptedBase64 = encryptionModule.decryptBase64(encryptedBase64);
+const encryptedBase64 = v2('encrypt', 'base64', { string: textToEncrypt });
+const decryptedBase64 = v2('decrypt', 'base64', { string: encryptedBase64 });
 
-const encryptedXOR = encryptionModule.encryptXOR(textToEncrypt, keyXOR);
-const decryptedXOR = encryptionModule.decryptXOR(encryptedXOR, keyXOR);
+const encryptedXOR = v2('encrypt', 'xor', { string: textToEncrypt, key: keyXOR });
+const decryptedXOR = v2('decrypt', 'xor', { string: encryptedXOR, key: keyXOR });
 
 console.log('Encrypted AES:', encryptedAES);
 console.log('Decrypted AES:', decryptedAES);
@@ -37,23 +34,14 @@ console.log('Decrypted XOR:', decryptedXOR);
 ```
 
 # Functions
-__`encryptAES(text: string, key: Buffer): Buffer`__
-Encrypts the input text using AES-256-GCM.
+__`v2(type: 'encrypt' | 'decrypt', method: 'aes' | 'base64' | 'xor', options?: { string: string | Buffer, key?: string | Buffer }): Buffer | string`__
+- Encrypts or decrypts based on the specified method.
+- __`type`__: 'encrypt' or 'decrypt'.
+- __`method`__: 'aes', 'base64' or 'xor'.
+- __`options`__: An object containing the properties required for the specified method.
 
-__`decryptAES(encodedData: Buffer, key: Buffer): string`__
-Decrypts the encoded data using AES-256-GCM.
-
-__`encryptBase64(text: string, key: string): string`__
-Encrypts the input text using base64.
-
-__`decryptBase64(encryptedText: string, key: string): string`__
-Decrypts the Base64-encoded text using base64.
-
-__`encryptXOR(text: string, key: number: string`__
-Encrypts the input text using XOR with the provided key.
-
-__`decryptXOR(encodedText: string, key: number): string`__
-Decrypts the XOR-encoded text using the provided key.
+__`v2.generateAESKey(keySize: number): Buffer`__
+- Generates a random AES key based on the specified size. 
 
 ## GitHub Repository
 https://www.github.com/CakeWithPineapple/encrypt
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,118 @@
 import * as crypto from 'crypto';
 
-export interface v1Interface {
-    encryptAES(text: string, key: Buffer): Buffer;
-    decryptAES(encodedData: Buffer, key: Buffer): string;
-    encryptBase64(text: string): string;
-    decryptBase64(encryptedText: string): string;
-    encryptXOR(text: string, key: number): string;
-    decryptXOR(encodedText: string, key: number): string;
+interface EncryptionOptions {
+  string: string | Buffer;
+  key?: Buffer | number;
 }
 
-const v1: v1Interface = {
-    encryptAES: (text, key) => {
-        const iv = crypto.randomBytes(12);
-        const cipher = crypto.createCipheriv('aes-256-gcm', key, iv);
+const error = (code: string, location: string, reason: string) => {
+  return `Exited with code ${code} due to ${reason} at ${location}.`;
+}
 
-        const ciphertext = Buffer.concat([cipher.update(text, 'utf-8'), cipher.final()]);
-        const tag = cipher.getAuthTag();
+v2.generateAESKey = (keySize: number): Buffer => {
+  return crypto.randomBytes(keySize / 8);
+}
 
-        return Buffer.concat([iv, ciphertext, tag]);
-    },
-    decryptAES: (encodedData: Buffer, key: Buffer): string => {
-        const nonce = encodedData.slice(0, 12); // Nonce length for AES-GCM is typically 12 bytes
-        const ciphertext = encodedData.slice(12, -16); // Exclude nonce and tag
-        const tag = encodedData.slice(-16); // Last 16 bytes is the authentication tag
-    
-        const decipher = crypto.createDecipheriv('aes-256-gcm', key, nonce);
-        decipher.setAuthTag(tag);
-    
-        try {
-            const decryptedBuffer = Buffer.concat([decipher.update(ciphertext), decipher.final()]);
-            return decryptedBuffer.toString('utf-8');
-        } catch (error: any) {
-            console.error('AES Decryption Error:', error);
-            return '';
-        }
-    },
-    encryptBase64: (text) => {
-        return btoa(text);
-    },
-    
-    decryptBase64: (encryptedText) => {
-        return atob(encryptedText);
-    },
-    
-    encryptXOR: (text, key) => {
-        let encodedText = "";
-        for (let i = 0; i < text.length; i++) {
-            const character = text.charCodeAt(i);
-            const encodedCharacter = String.fromCharCode(character ^ key);
-            encodedText += encodedCharacter;
-        }
-        return encodedText;
-    },
-    
-    decryptXOR(encodedText: string, key: number): string {
-        return this.encryptXOR(encodedText, key);
-    }    
-};
+type Method = 'aes' | 'base64' | 'xor';
+  
+export function v2(type: 'encrypt' | 'decrypt', method: Method, options: EncryptionOptions): Buffer | string {
+  const location = `${method.toUpperCase()}_${type.toUpperCase()}`;
+  
 
-export default v1;
+  if (method === 'aes' && type === 'encrypt') {
+    if (!(options.key instanceof Buffer) || typeof options.string !== "string") {
+      throw new Error(`${error("40T", location, "invalid type")}`);
+    } else {
+      return encryptAES(options.string, options.key);
+    }
+  }
+  if (method === 'base64' && type === 'encrypt') {
+    if (typeof options.string !== "string") {
+      throw new Error(`${error("40T", location, "invalid type")}`);
+    } else if (options.key) {
+      throw new Error(`${error("40N", location, "non-existent prop \"options.key\"")}`)
+    } else {
+      return encryptBase64(options.string);
+    }
+  }
+  if (method === 'xor' && type === 'encrypt') {
+    if (typeof options.string !== "string" || typeof options.key !== "number") {
+      throw new Error(`${error("40T", location, "invalid type")}`);
+    } else {
+      return encryptXOR(options.string, options.key);
+    }
+  }
+  if (method === 'aes' && type === 'decrypt') {
+    if (!(options.key instanceof Buffer) || !(options.string instanceof Buffer)) {
+      throw new Error(`${error("40T", location, "invalid type")}`);
+    } else {
+      return decryptAES(options.string, options.key);
+    }
+  }
+  if (method === 'base64' && type === 'decrypt') {
+    if (typeof options.string !== "string") {
+      throw new Error(`${error("40T", location, "invalid type")}`);
+    } else if (options.key) {
+      throw new Error(`${error("40N", location, "non-existent prop \"options.key\"")}`)
+    } else {
+      return decryptBase64(options.string);
+    }
+  }
+  if (method === 'xor' && type === 'decrypt') {
+    if (typeof options.string !== "string" || typeof options.key !== "number") {
+      throw new Error(`${error("40T", location, "invalid type")}`);
+    } else {
+      return v2("encrypt", 'xor', {
+        string: options.string,
+        key: options.key,
+      });
+    }
+  }
+
+  return `${error("40U", "v2", "that no type or method was specified")}`;
+}
+
+function encryptAES(text: string, key: Buffer) {
+  const iv = crypto.randomBytes(12);
+  const cipher = crypto.createCipheriv('aes-256-gcm', key, iv);
+
+  const ciphertext = Buffer.concat([cipher.update(text, 'utf-8'), cipher.final()]);
+  const tag = cipher.getAuthTag();
+
+  return Buffer.concat([iv, ciphertext, tag]);
+}
+
+function encryptBase64(text: string) {
+  return btoa(text);
+}
+
+function encryptXOR(text: string, key: number) {
+  let encodedText = "";
+  for (let i = 0; i < text.length; i++) {
+      const character = text.charCodeAt(i);
+      const encodedCharacter = String.fromCharCode(character ^ key);
+      encodedText += encodedCharacter;
+  }
+  return encodedText;
+}
+
+function decryptAES(data: Buffer, key: Buffer): string {
+  const nonce = data.slice(0, 12); // Nonce length for AES-GCM is typically 12 bytes
+  const ciphertext = data.slice(12, -16); // Exclude nonce and tag
+  const tag = data.slice(-16); // Last 16 bytes is the authentication tag
+
+  const decipher = crypto.createDecipheriv('aes-256-gcm', key, nonce);
+  decipher.setAuthTag(tag);
+
+  try {
+    const decryptedBuffer = Buffer.concat([decipher.update(ciphertext), decipher.final()]);
+    return decryptedBuffer.toString('utf-8');
+  } catch (error: any) {
+    console.error('AES Decryption Error:', error);
+    return '';
+  }
+}
+
+function decryptBase64(text: string) {
+  return atob(text);
+}
